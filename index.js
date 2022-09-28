2a2b23c8e70399c0cfbb9be49d094ceb9ad2d77b
DuongDo2410 <66940364+DuongDo2410@users.noreply.github.com>
2022-09-28 14:06:18 +0700

    firt commit

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const morgan = require("morgan");
+
+const app = express();
+dotenv.config();
+app.use(cors());
+app.use(morgan("tiny"));
+
+const DB_URL = process.env.DATABASE_MONGO ? process.env.DATABASE_MONGO : "";
+mongoose.connect(DB_URL).then(() => {
+  console.log("DB Connected");
+});
+mongoose.connection.on("error", (err) => {
+  console.log(`DB Connected error: ${err.message}`);
+});
+
+app.get("/", (req, res) => {
+  res.json({ product: { name: "name" } });
+});
+
+const PORT = process.env.APP_PORT || 8000;
+app.listen(PORT, () => {
+  console.log("sever is running port ", PORT);
+});
